a63c4d129b078d4e6505fd253dbc6afc45bdf482
Inam Khan <149032309+inam0980@users.noreply.github.com>
2026-01-24 10:46:15 +0530

    Time: 2 ms (2.43%) | Memory: 44.3 MB (5.40%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,20 +1,24 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        boolean a=true;
+
+        ArrayList<ArrayList<Integer>> lst= new ArrayList<>();
 
         for(int i=0;i<matrix.length;i++){
+            ArrayList<Integer> a= new ArrayList<>();
             for(int j=0;j<matrix[i].length;j++){
-                if(matrix[i][j]==target){
-                    return a;
-                }
+                a.add(matrix[i][j]);
+            }
+            lst.add(a);
+        }
+        for(int i=0;i<lst.size();i++){
+            Collections.sort(lst.get(i));
+        }
+        for (int i = 0; i < lst.size(); i++) {
+            if (Collections.binarySearch(lst.get(i), target)>=0) { 
+                return true;
             }
         }
         return false;
-
-
-
-
-
-        
+      
     }
 }
