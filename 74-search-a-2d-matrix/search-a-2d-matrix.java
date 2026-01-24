53acf7784840b69cd078667146f148efd795cdb3
Inam Khan <149032309+inam0980@users.noreply.github.com>
2026-01-24 10:20:50 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (16.20%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        boolean a=true;
+
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[i].length;j++){
+                if(matrix[i][j]==target){
+                    return a;
+                }
+            }
+        }
+        return false;
+
+
+
+
+
+        
+    }
+}
