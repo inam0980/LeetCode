2292b7a93137e1d1ce061321b3f0d3323b4bf169
Inam Khan <149032309+inam0980@users.noreply.github.com>
2026-01-25 03:59:26 +0530

    Time: 1 ms (99.84%) | Memory: 47.1 MB (37.75%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,22 +1,38 @@
+
 class Solution {
     public int singleNumber(int[] nums) {
-
-        
-            for(int i=0;i<nums.length;i++){
-                int k=0;
-                for(int j=0;j<nums.length;j++){
-                    if(nums[i]==nums[j]){
-                        
-                        k+=1;
-                    }
-                }
-                if(k==1){
-                    return nums[i];
-                }
-            }
-        
-        return nums[0];
-
-        
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            ans = ans ^ nums[i];
+        }
+        return ans;
     }
 }
+
+
+
+
+
+
+// class Solution {
+//     public int singleNumber(int[] nums) {
+
+        
+//             for(int i=0;i<nums.length;i++){
+//                 int k=0;
+//                 for(int j=0;j<nums.length;j++){
+//                     if(nums[i]==nums[j]){
+                        
+//                         k+=1;
+//                     }
+//                 }
+//                 if(k==1){
+//                     return nums[i];
+//                 }
+//             }
+        
+//         return nums[0];
+
+        
+//     }
+// }
