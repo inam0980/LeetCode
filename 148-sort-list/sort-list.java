29f88677a58723ba448d38d07348d16d941df222
Inam Khan <149032309+inam0980@users.noreply.github.com>
2026-02-16 11:37:11 +0530

    Time: 15 ms (14.40%) | Memory: 64.5 MB (8.81%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -1,24 +1,23 @@
-
 class Solution {
     public ListNode sortList(ListNode head) {
 
-        ArrayList<Integer> lst=new ArrayList<>();
+        ArrayList<Integer> lst = new ArrayList<>();
 
-        while(head!=null){
+        while (head != null) {
             lst.add(head.val);
-            head=head.next;
+            head = head.next;
         }
+
         Collections.sort(lst);
 
-        ListNode temp=new ListNode(0);
-        ListNode temp2=temp;
+        ListNode temp = new ListNode(0);
+        ListNode temp2 = temp;
 
-        for(int x: lst){
-            ListNode newnode= new ListNode(x);
-            temp2.next=newnode;
-            temp2=temp2.next;
+        for (int x : lst) {
+            temp2.next = new ListNode(x);
+            temp2 = temp2.next;
         }
+
         return temp.next;
-        
     }
 }
