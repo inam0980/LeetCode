7109b1a9c874d535880ad8962f0b1b81a4ea8077
Inam Khan <149032309+inam0980@users.noreply.github.com>
2026-02-19 13:35:52 +0530

    Time: 2 ms (15.17%) | Memory: 116.2 MB (61.80%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
@@ -4,17 +4,19 @@ class Solution {
         int max=0;
 
         int arr[]={1,2};
+        int a=0,b=0;
 
         for(int i=0;i<mat.length;i++){
             for(int j=0;j<mat[0].length;j++){
                 if(mat[i][j]>max){
                     max=mat[i][j];
-                    arr=new int[]{i,j};
+                    a=i;
+                    b=j;
 
                 }
             }
         }
-        return arr;
+        return new int[]{a,b};
         
         
     }
