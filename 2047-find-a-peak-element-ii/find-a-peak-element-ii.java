8907b7de19ee21fb4fe51101d2a3bc17d9358417
Inam Khan <149032309+inam0980@users.noreply.github.com>
2026-02-19 13:34:52 +0530

    Time: 2 ms (15.17%) | Memory: 116.4 MB (22.13%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
@@ -1,24 +1,21 @@
 class Solution {
     public int[] findPeakGrid(int[][] mat) {
 
-        int max=mat[0][0];
+        int max=0;
 
-        int p=0,k=0;
-       
+        int arr[]={1,2};
+
         for(int i=0;i<mat.length;i++){
             for(int j=0;j<mat[0].length;j++){
                 if(mat[i][j]>max){
                     max=mat[i][j];
-                    p=i;
-                    k=j;
+                    arr=new int[]{i,j};
 
                 }
-
             }
         }
-        int arr[]={p,k};
-
         return arr;
+        
         
     }
 }
